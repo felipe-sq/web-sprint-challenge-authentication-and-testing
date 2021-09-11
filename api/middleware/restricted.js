0ea8cb70904a5c3f39ff2d2d22e671004f423f6a
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-09-10 20:01:39 -0400

    ran migrate:latest, wrote code for restricted middleware with jsonwebtoken and JWT_SECRET

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,21 @@
+const { JWT_SECRET } = require('../secrets/index.js');
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'token required' });
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: 'token invalid' }); 
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  }
+    /*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
